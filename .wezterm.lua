9b7629570692d1e8a3676cc6eb29b1e4894a5dd1
GeordieEK <geordieek@gmail.com>
2024-10-11 09:48:21 +1100

    confirm tmux exit

--- .wezterm.lua
+++ .wezterm.lua
@@ -16,53 +16,69 @@ config.color_scheme = "Catppuccin Mocha"
 -- Use powerlevel10k recommended font
 config.font = wezterm.font("MesloLGS NF")
 
+-- Always prompt window close (tmux handles panes anyway)
+config.window_close_confirmation = "AlwaysPrompt"
+
 local act = wezterm.action
 
--- Set keybindings
-config.keys = {
-	-- Split the pane horizontally (top/bottom) with CMD + SHIFT + J
-	{
-		key = "J",
-		mods = "CMD|SHIFT",
-		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
-	},
-	-- Split the pane vertically (left/right) with CMD + SHIFT + J
-	{
-		key = "L",
-		mods = "CMD|SHIFT",
-		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
-	},
-	-- Close the current pane with CMD + SHIFT + K
-	{
-		key = "K",
-		mods = "CMD|SHIFT",
-		action = act.CloseCurrentPane({ confirm = false }),
-	},
-	-- Navigate to the pane above with CMD + K
-	{
-		key = "k",
-		mods = "CMD",
-		action = act.ActivatePaneDirection("Up"),
-	},
-	-- Navigate to the pane below with CMD + J
-	{
-		key = "j",
-		mods = "CMD",
-		action = act.ActivatePaneDirection("Down"),
-	},
-	-- Navigate to the pane to the right with CMD + L
-	{
-		key = "l",
-		mods = "CMD",
-		action = act.ActivatePaneDirection("Right"),
-	},
-	-- Navigate to the pane to the right with CMD + H
-	{
-		key = "h",
-		mods = "CMD",
-		action = act.ActivatePaneDirection("Left"),
-	},
+-- This overrides the default so wezterm will ask for confirmation when closing tmux
+config.skip_close_confirmation_for_processes_named = {
+	"bash",
+	"sh",
+	"zsh",
+	"fish",
+	"nu",
+	"cmd.exe",
+	"pwsh.exe",
+	"powershell.exe",
 }
+
+-- Set keybindings for pane navigation (uncommented as this is now handled with tmux)
+
+-- config.keys = {
+-- 	-- Split the pane horizontally (top/bottom) with CMD + SHIFT + J
+-- 	{
+-- 		key = "J",
+-- 		mods = "CMD|SHIFT",
+-- 		action = act.SplitVertical({ domain = "CurrentPaneDomain" }),
+-- 	},
+-- 	-- Split the pane vertically (left/right) with CMD + SHIFT + J
+-- 	{
+-- 		key = "L",
+-- 		mods = "CMD|SHIFT",
+-- 		action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }),
+-- 	},
+-- 	-- Close the current pane with CMD + SHIFT + K
+-- 	{
+-- 		key = "K",
+-- 		mods = "CMD|SHIFT",
+-- 		action = act.CloseCurrentPane({ confirm = false }),
+-- 	},
+-- 	-- Navigate to the pane above with CMD + K
+-- 	{
+-- 		key = "k",
+-- 		mods = "CMD",
+-- 		action = act.ActivatePaneDirection("Up"),
+-- 	},
+-- 	-- Navigate to the pane below with CMD + J
+-- 	{
+-- 		key = "j",
+-- 		mods = "CMD",
+-- 		action = act.ActivatePaneDirection("Down"),
+-- 	},
+-- 	-- Navigate to the pane to the right with CMD + L
+-- 	{
+-- 		key = "l",
+-- 		mods = "CMD",
+-- 		action = act.ActivatePaneDirection("Right"),
+-- 	},
+-- 	-- Navigate to the pane to the right with CMD + H
+-- 	{
+-- 		key = "h",
+-- 		mods = "CMD",
+-- 		action = act.ActivatePaneDirection("Left"),
+-- 	},
+-- }
 
 -- and finally, return the configuration to wezterm
 return config
